46b27b51d1f34f480f4f9c1706c1292f77d693b1
Jeffery William Martin <j.martin@uwinnipeg.ca>
2020-08-31 19:51:25 -0500

    Added a plot of irreversible T2 to the reversible T2 graph, for comparison.

--- plot_visibility.py
+++ plot_visibility.py
@@ -6,19 +6,22 @@
 
 from visibility import *
 
-print(gamma_n)
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 g10=np.arange(-30,30,0.01) # pT/cm
 alpha=alpha_rev(g10*1e-12*1e+2)
-plt.plot(g10,alpha)
+plt.plot(g10,alpha,label='Eq. (22) -- reversible Ramsey wrapping')
 plt.title('UCN polarization after T = 180 s storage')
 plt.xlim(-60,60)
 plt.ylim(0.6,0.91)
 plt.xlabel(r'$G_{1,0}$ (pT/cm)')
 plt.ylabel(r'$\alpha$')
+# add a plot of the irreversible T2 from Fig. 2
+g10=np.arange(-60,60,0.01) # pT/cm
+alpha=alpha_irrev(0,0,g10*1e-12*1e+2)
+plt.plot(g10,alpha,label='Eq. (29) -- irreversible/intrinsic T2')
+plt.legend()
 plt.show()
 
 g1p1=np.arange(-300,300,0.01) # pT/cm
